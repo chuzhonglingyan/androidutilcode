c5792ba06beaa86c4b15819fe5788f14e8aae2e2
guangleilei <944610721@qq.com>
2017-03-22 00:07:43 +0800

    增加zxing

--- baselibs/src/main/java/com/yutian/common/adpater/OnItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.yutian.common.adpater;
-
-import android.view.View;
-import android.view.ViewGroup;
-
-public interface OnItemClickListener<T>
-{
-    void onItemClick(ViewGroup parent, View view, T t, int position);
-}
